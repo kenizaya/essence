88bfce6863f5e1dc2b33197fd09e93206ca72a09
kenizaya <harveylstark@gmail.com>
2023-10-01 15:34:39 +0530

    Add steps section and file upload preview image in landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,11 +19,6 @@ export default function Home() {
         </h1>
         <p className='mt-5 max-w-prose text-zinc-700 sm:text-lg'>
           Essence allows you to have conversations with your PDF.{' '}
-          <ol>
-            <li>Upload your file.</li>
-            <li>Ask your questions.</li>
-          </ol>{' '}
-          It&apos;s that simple.
         </p>
 
         <Link
@@ -97,6 +92,66 @@ export default function Home() {
             <p className='mt-4 text-lg text-gray-600'>
               Chatting with your PDF has never been easier.
             </p>
+          </div>
+        </div>
+
+        {/* Steps */}
+
+        <ol className='my-8 space-y-4 pt-8 md:flex md:space-x-12 md:space-y-0'>
+          <li className='md:flex-1'>
+            <div className='flex flex-col space-y-2 border-l-4 border-zinc-300 py-2 pl-4 md:border-l-0 md:border-t-2 md:pb-0 md:pl-0 md:pt-4'>
+              <span className='text-sm font-medium text-blue-600'>Step 1</span>
+              <span className='text-xl font-semibold'>
+                Sign up for an account
+              </span>
+              <span className='mt-2 text-zinc-700'>
+                Start out with a free account or get our{' '}
+                <Link
+                  href={'/pricing'}
+                  className='text-blue-700 underline underline-offset-2'
+                >
+                  pro plan
+                </Link>
+                .
+              </span>
+            </div>
+          </li>
+          <li className='md:flex-1'>
+            <div className='flex flex-col space-y-2 border-l-4 border-zinc-300 py-2 pl-4 md:border-l-0 md:border-t-2 md:pb-0 md:pl-0 md:pt-4'>
+              <span className='text-sm font-medium text-blue-600'>Step 2</span>
+              <span className='text-xl font-semibold'>Upload your PDF</span>
+              <span className='mt-2 text-zinc-700'>
+                Essence will process your file and make it ready for you to chat
+                with.
+              </span>
+            </div>
+          </li>
+          <li className='md:flex-1'>
+            <div className='flex flex-col space-y-2 border-l-4 border-zinc-300 py-2 pl-4 md:border-l-0 md:border-t-2 md:pb-0 md:pl-0 md:pt-4'>
+              <span className='text-sm font-medium text-blue-600'>Step 3</span>
+              <span className='text-xl font-semibold'>
+                Start asking questions
+              </span>
+              <span className='mt-2 text-zinc-700'>
+                It&apos;s that simple. Try Essence today - It takes less than a
+                minute.
+              </span>
+            </div>
+          </li>
+        </ol>
+
+        <div className='mx-auto max-w-6xl px-6 lg:px-8'>
+          <div className='mt-16 flow-root sm:mt-24'>
+            <div className='-m-2 rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:-m-4 lg:rounded-2xl lg:p-4'>
+              <Image
+                src='/file-upload-preview.webp'
+                alt='uploading preview'
+                width={1419}
+                height={732}
+                quality={100}
+                className='rounded-md bg-white p-2 sm:p-8 md:p-20 shadow-2xl ring-1 ring-gray-900/10'
+              />
+            </div>
           </div>
         </div>
       </div>
