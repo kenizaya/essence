1953c3c3b78ec80e1a7d8626a54ab6d45928c66a
kenizaya <harveylstark@gmail.com>
2023-12-17 19:42:26 +0530

    Payment integration

--- src/components/UpgradeButton.tsx
+++ src/components/UpgradeButton.tsx
@@ -1,10 +1,19 @@
+'use client'
+
 import React from 'react'
 import { Button } from './ui/button'
 import { ArrowRight } from 'lucide-react'
+import { trpc } from '@/app/_trpc/client'
 
 const UpgradeButton = () => {
+  const { mutate: createStripeSession } = trpc.createStripeSession.useMutation({
+    onSuccess: ({ url }) => {
+      window.location.href = url ?? '/dashboard/billing'
+    },
+  })
+
   return (
-    <Button className='w-full'>
+    <Button onClick={() => createStripeSession()} className='w-full'>
       Upgrade Now <ArrowRight className='h-5 w-5 ml-1.5' />
     </Button>
   )
