7d5adf333b50ed65409b536ad4965a9f76443400
PrashantT <prashant.thapa@undef.studio>
2024-03-07 17:48:51 +0530

    Dark mode for dashboard and fix alignment

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -31,9 +31,11 @@ const Dashboard = ({ subscriptionPlan }: DashboardProps) => {
   })
 
   return (
-    <main className='mx-auto max-w-7xl md:p-10'>
-      <div className='mt-8 flex flex-col items-start justify-between gap-4 border-b border-gray-200 pb-5 sm:flex-row sm:items-center sm:gap-0'>
-        <h1 className='mb-3 font-bold text-5xl text-gray-900'>My Files</h1>
+    <main className='mx-auto max-w-7xl md:p-10 md:px-20 p-5'>
+      <div className='mt-8 flex flex-row items-start justify-between gap-4 border-b border-gray-200 pb-5 items-center sm:gap-0'>
+        <h1 className='mb-3 font-bold text-5xl dark:text-neutral-100 text-gray-900'>
+          My Files
+        </h1>
 
         <UploadButton isSubscribed={subscriptionPlan.isSubscribed} />
       </div>
@@ -97,7 +99,7 @@ const Dashboard = ({ subscriptionPlan }: DashboardProps) => {
         <Skeleton height={100} className='my-2' count={3} />
       ) : (
         <div className='mt-16 flex flex-col items-center gap-2'>
-          <Ghost className='h-8 w-8 text-zinc-800' />
+          <Ghost className='h-8 w-8 dark:text-neutral-300 text-zinc-800' />
           <h3 className='font-semibold text-xl'>Pretty empty around here</h3>
           <p>Let&apos;s upload your first PDF.</p>
         </div>
