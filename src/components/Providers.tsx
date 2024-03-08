21bb5fa5ad5e1f8a018efc94423e37d38a20756d
PrashantT <104677763+pshdotgg@users.noreply.github.com>
2024-03-08 09:35:23 +0530

    Show a progress bar at the top when changing routes

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -5,6 +5,7 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { trpc } from '@/app/_trpc/client'
 import { httpBatchLink } from '@trpc/react-query'
 import { absoluteUrl } from '@/lib/utils'
+import { AppProgressBar as ProgressBar } from 'next-nprogress-bar'
 
 const Providers = ({ children }: PropsWithChildren) => {
   const [queryClient] = useState(() => new QueryClient())
@@ -19,7 +20,15 @@ const Providers = ({ children }: PropsWithChildren) => {
   )
   return (
     <trpc.Provider client={trpcClient} queryClient={queryClient}>
-      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+      <QueryClientProvider client={queryClient}>
+        {children}
+        <ProgressBar
+          height='2px'
+          color='#667eea'
+          options={{ showSpinner: false }}
+          shallowRouting
+        />
+      </QueryClientProvider>
     </trpc.Provider>
   )
 }
