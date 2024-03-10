c5f61429da7564f724a2ff4549a81eae54b255b9
PrashantT <104677763+pshdotgg@users.noreply.github.com>
2024-03-10 20:01:01 +0530

    Responsive gemini

--- src/components/DarkModeToggle.tsx
+++ src/components/DarkModeToggle.tsx
@@ -17,8 +17,8 @@ export function DarkModeToggle(props: React.ComponentProps<typeof Button>) {
   }
   return (
     <Button onClick={handleClick} variant='ghost' size='icon' {...props}>
-      <SunMedium className='h-[1.1rem] dark:text-neutral-300 w-[1.1rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0' />
-      <MoonStar className='absolute sm:dark:text-neutral-100 dark:text-neutral-300 h-[1.1rem] w-[1.1rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100' />
+      <SunMedium className='h-[1rem] dark:text-neutral-300 w-[1rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0' />
+      <MoonStar className='absolute sm:dark:text-neutral-100 dark:text-neutral-300 h-[1rem] w-[1rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100' />
       <span className='sr-only'>Toggle theme</span>
     </Button>
   )
